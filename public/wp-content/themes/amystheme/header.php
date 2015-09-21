4df453b4588652fb5b357ab9f9248051cb9b88db
Amy Evans <amyevans87@gmail.com>
2015-09-21 15:00:03 -0500

    Clean up header/nav

--- public/wp-content/themes/amystheme/header.php
+++ public/wp-content/themes/amystheme/header.php
@@ -4,12 +4,8 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
 		<title><?php wp_title(' | ', true, 'right'); ?></title>
-
 		<link href="<?php bloginfo('stylesheet_url') ?>" rel="stylesheet">
-
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -24,20 +20,19 @@
 		<div class="container-fluid">
 			<!-- Brand and toggle get grouped for better mobile display -->
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
 					<span class="sr-only">Toggle navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-			<a class="navbar-brand" href="#">Brand</a>
+			<a href="<?php echo home_url('/')?>" class="navbar-brand"><?php bloginfo('name')?></a>
 			</div>
 			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<div class="collapse navbar-collapse" id="navbar-collapse">
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#">Link</a></li>
-					<li><a href="#">Link</a></li>
-					<li><a href="#">Link</a></li>
+					<!-- Wordpress gets the pages and puts them in the navigation -->
+					<?php wp_list_pages('title_li='); ?>
 				</ul>
 		  </div><!-- /.navbar-collapse -->
 		</div><!-- /.container-fluid -->
--- public/wp-content/themes/amystheme/index.php
+++ public/wp-content/themes/amystheme/index.php
@@ -1,3 +1,18 @@
 <?php get_header(); ?>
 
+<div class="container">
+	<div class="row">
+	<?php 
+		query_posts('posts_per_page=3');
+			while(have_posts()) : the_post(); ?>
+
+		<div class="col-sm-4">
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<p><?php the_excerpt(); ?></p>
+		</div><!-- /.col-lg-3 -->
+
+	<?php endwhile; wp_reset_query(); ?>	
+	</div><!-- /.row -->	
+</div><!-- /.container -->
+
 <?php get_footer(); ?>
